ce006558948f4edfeae2a7210cd72978bc3e6e28
Texisde <97799540+Texisde@users.noreply.github.com>
2022-04-21 15:38:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //coloquei o enunciado como alert para aparecer na tela
 alert("Quiz dos personagens do anime Shingeki no Kyojin")
 
+
 //aqui coloquei um array dentro de uma variável para ficar mais facil utilizar os elementos
 var personagem = ["Mikasa", "Levi", "Eren"]
 
@@ -14,15 +15,15 @@ var texto = "Seu personagem tem como caracteristicas:\n"
 switch(indice){
     case personagem[2]: //aqui puxei o index do personagem q ta dentro da var indice
         alert( texto + "TATAKAE\n" + "Titã de ataque")
-        var teste = prompt("Digite o nome do personagem:")
+        var teste = prompt("Digite o nome do personagem com a primeira letra maiúscula:")
         break
     case personagem[0]: //aqui puxei o index do personagem q ta dentro da var indice
         alert( texto + "Ackerman\nMulher")
-        var option = prompt("Digite o nome do personagem: ")
+        var option = prompt("Digite o nome do personagem com a primeira letra maiúscula:")
         break
     case personagem[1]: //aqui puxei o index do personagem q ta dentro da var indice
         alert( texto + "Capitão\n" + "Mais forte da humanidade")
-        var lad = prompt("Digite o nome do personagem:")
+        var lad = prompt("Digite o nome do personagem com a primeira letra maiúscula:")
         break
     default:
         alert("ERRO")
